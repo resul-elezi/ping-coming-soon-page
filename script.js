d7f1736ddb744abe1020dcb5239a6ac4cffc7466
Resul Elezi <resul_elezi@protonmail.com>
2023-11-18 08:02:58 +0100

    #

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
 "use strict";
 
-const VALIDATE_FORM = formSelector => {
-    const FORM_ELEMENT = document.querySelector(formSelector);
+const FORM = document.getElementById("form");
+const EMAIL = document.getElementById("email");
+const ERROR = document.querySelector(".error-msg");
 
-const VALIDATE_MAIL = mail => {
-    const INPUT = mail.querySelector("input");
-    const ERROR_CONTAINER = mail.querySelector(".error-msg");
-
-const VALIDATE_OPTIONS = [
-    {
-        attribute: "pattern",
-        isValid: INPUT => {
-            const PATTERN_REGEX = new RegExp(INPUT.pattern);
-            return PATTERN_REGEX.test(INPUT.value);
-        },
-        errorMessage: (INPUT) => `Please provide a valid ${INPUT.placeholder}`
+FORM.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(!validateEmail(EMAIL_INPUT.value)) {
+        ERROR.classList.remove("hidden");
+        ERROR.classList.remove("hidden");
+        EMAIL.style.border = "1px solid hsl(354, 100%, 66%)";
     }
-];
-// const VALIDATE_SINGLE_FORM_GROUP = formGroup => {
-//     const INPUT = formGroup.querySelector("input");
-//     const ERROR_CONTAINER = formGroup.querySelector(".error-msg");
-
-let emailError = false;
-for(const OPTION of VALIDATE_OPTIONS) {
-    if(INPUT.hasAttribute(OPTION.attribute) && !OPTION.isValid(INPUT)) {
-        ERROR_CONTAINER.textContent = OPTION.errorMessage(INPUT);
-        ERROR_CONTAINER.classList.remove("hidden");
-        INPUT.style.border = "1px solid hsl(354, 100%, 66%)";
-        emailError = true;
-    }
-}
-// }
-if(!emailError) {
-    ERROR_CONTAINER.classList.add("hidden");
-    INPUT.style.border = "";
-}
-};
-FORM_ELEMENT.setAttribute("novalidate", "");
-
-
-FORM_ELEMENT.addEventListener("submit", event => {
-    event.preventDefault();
 });
 
+const SET_ERROR = 
+
+const VALIDATE_INPUT = () => {
+    const EMAIL_VALUE = EMAIL.value.trim();
 };
 
-VALIDATE_FORM("#form");
+function validateEmail(EMAIL_INPUT) {
+    const PATTERN = /^[A-Za-z\._\-0-9]*\[@\][A-Za-z]*\[\.\][a-z]{2,4}$/;
+    return PATTERN.test(EMAIL_INPUT);
+}
+
+// const ERROR_CONTAINER = formGroup.querySelector(".error-msg");
+
+// let emailError = false;
+// for(const OPTION of VALIDATE_OPTIONS) {
+//     if(INPUT.hasAttribute(OPTION.attribute) && !OPTION.isValid(INPUT)) {
+//         ERROR_CONTAINER.textContent = OPTION.errorMessage(INPUT);
+//         ERROR_CONTAINER.classList.remove("hidden");
+//         INPUT.style.border = "1px solid hsl(354, 100%, 66%)";
+//         emailError = true;
+//     }
+// }
+// // }
+// if(!emailError) {
+//     ERROR_CONTAINER.classList.add("hidden");
+//     INPUT.style.border = "";
+// }
+
 
