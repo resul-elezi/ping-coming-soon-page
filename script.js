8431cb441ce324b34eb7a29f1a78ee3c4df80dca
Resul Elezi <resul_elezi@protonmail.com>
2023-11-18 17:42:54 +0100

    #

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
 "use strict";
 
-const FORM = document.getElementById("form");
 const EMAIL = document.getElementById("email");
+const BUTTON = document.getElementById("button");
 const ERROR = document.querySelector(".error-msg");
 
-FORM.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if(!validateEmail(EMAIL_INPUT.value)) {
-        ERROR.classList.remove("hidden");
-        ERROR.classList.remove("hidden");
+
+BUTTON.addEventListener('click', (e) => {
+    // e.preventDefault(); // prevent the auto submission of the form
+
+    const REG =  /^[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]+(\.[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]+)*$/;
+
+    if(EMAIL.value.match(REG)){
+        ERROR.style.display = "none";
+        EMAIL.style.border = "";
+    } 
+    else {
+        ERROR.style.display = "block"
+        ERROR.innerText = "Please provide a valid email address";
         EMAIL.style.border = "1px solid hsl(354, 100%, 66%)";
     }
-});
-
-const SET_ERROR = 
-
-const VALIDATE_INPUT = () => {
-    const EMAIL_VALUE = EMAIL.value.trim();
-};
-
-function validateEmail(EMAIL_INPUT) {
-    const PATTERN = /^[A-Za-z\._\-0-9]*\[@\][A-Za-z]*\[\.\][a-z]{2,4}$/;
-    return PATTERN.test(EMAIL_INPUT);
-}
-
-// const ERROR_CONTAINER = formGroup.querySelector(".error-msg");
-
-// let emailError = false;
-// for(const OPTION of VALIDATE_OPTIONS) {
-//     if(INPUT.hasAttribute(OPTION.attribute) && !OPTION.isValid(INPUT)) {
-//         ERROR_CONTAINER.textContent = OPTION.errorMessage(INPUT);
-//         ERROR_CONTAINER.classList.remove("hidden");
-//         INPUT.style.border = "1px solid hsl(354, 100%, 66%)";
-//         emailError = true;
-//     }
-// }
-// // }
-// if(!emailError) {
-//     ERROR_CONTAINER.classList.add("hidden");
-//     INPUT.style.border = "";
-// }
+})
 
 
