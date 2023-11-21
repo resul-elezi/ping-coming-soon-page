46ba23a158b7a5af4662e50380543aeb5f3899a9
re-d-e-v <resul_elezi@protonmail.com>
2023-11-21 18:47:14 +0100

    #

--- script.js
+++ script.js
@@ -3,19 +3,28 @@
 const EMAIL = document.getElementById("email");
 const BUTTON = document.getElementById("button");
 const FORM = document.getElementById("form");
+const ERROR_P = document.querySelector(".msg");
+
 
 BUTTON.addEventListener('click', (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     
     const REG_EX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
-    if (!EMAIL.value.match(REG_EX)) {
+
+    if(EMAIL.value === "") {
         FORM.classList.add("error");
-    } 
-    else {
+        ERROR_P.innerHTML = "Email is required";
+    } else if(!EMAIL.value.match(REG_EX)) {
+        FORM.classList.add("error");
+        ERROR_P.innerHTML = "Please provide a valid email address";
+    } else {
         FORM.classList.remove("error");
     }
 });
+
+
+
+
 
 
 
